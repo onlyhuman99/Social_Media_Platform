c3335c5e6668416bfc27dcfca84c2b338c17c8c8
onlyhuman99 <kpardhu453@gmail.com>
2025-09-24 09:51:56 +0530

    sixth commit

--- social-frontend/src/components/Signup.jsx
+++ social-frontend/src/components/Signup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import api from '../api/api';
-import "../styles/SignUp.css";
+import "../styles/Signup.css";
 
 function Signup() {
   const navigate = useNavigate();
